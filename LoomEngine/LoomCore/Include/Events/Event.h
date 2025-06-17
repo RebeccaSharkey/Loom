00c9f1f232a517f25e8f72661ad1267771f371a4
Rebecca Sharkey <rebeccajaynesharkey@gmail.com>
2025-06-17 23:44:16 +0100

    Bug Fixes

--- LoomEngine/LoomCore/Include/Events/Event.h
+++ LoomEngine/LoomCore/Include/Events/Event.h
@@ -30,9 +30,9 @@ namespace Loom
     };
 
 #define EVENT_CLASS_TYPE(type) \
-    static EventType GetStaticType() { return EventType::##type; } \
+    static EventType GetStaticType() { return EventType::type; } \
     virtual EventType GetEventType() const override { return GetStaticType();} \
-    virtual const char* GetName() const override { ##type }\
+    virtual const char* GetName() const override { #type; }\
 
 #define EVENT_CLASS_CATEGORY(category) \
     virtual int GetCategoryFlags() const override { return category; }
